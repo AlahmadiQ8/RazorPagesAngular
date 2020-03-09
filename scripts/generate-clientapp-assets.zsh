707d14fe5802e84d05474b8f9f5ed97249ccbdef
Mohammad Mohammad <m91.alahmadi@gmail.com>
2020-03-09 18:35:34 -0400

    initial

---
+++ scripts/generate-clientapp-assets.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+pathToIndex="ClientApp/dist/index.html";
+lineToStyleSheets=$(awk '/stylesheet/{ print NR; exit }' $pathToIndex );  # DANGER Super Brittle
+lineToScripts=$(awk '/script/{ print NR; exit }' $pathToIndex );          # DANGER Super Brittle
+pathToPartials="Pages/Shared"
+
+styleSheets=$(sed "${lineToStyleSheets}q;d" $pathToIndex);
+styleSheets=$(echo $styleSheets | sed 's/<\/head>//g' );
+scripts=$(sed "${lineToScripts}q;d" $pathToIndex);
+scripts=$(echo $scripts | sed 's/<\/body>//g' );
+
+echo $styleSheets > "$pathToPartials/_AppStyleSheetsProd.cshtml";
+echo $scripts > "$pathToPartials/_AppScriptsProd.cshtml"
+echo "Created prod prartials for _AppStyleSheetsProd.cshtml & _AppScriptsProd.cshtml";
